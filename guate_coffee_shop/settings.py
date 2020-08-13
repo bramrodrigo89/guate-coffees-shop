9ce0c731139d23d97c7e222c9b1babe4a1eccf40
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-08-13 21:31:54 +0000

    added contexts.py in cart app and added context to all aps in settings

--- guate_coffee_shop/settings.py
+++ guate_coffee_shop/settings.py
@@ -91,6 +91,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
+                'cart.contexts.cart_items',
             ],
         },
     },
