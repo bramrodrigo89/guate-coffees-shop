70868ed058fe5cea1f5661aee1707107f0ec1b76
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-08-14 02:24:14 +0000

    Added add to cart functionality connecting the session storage

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,11 +1,27 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def cart_items(request):
 
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('get', {})
+    print(cart)
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.retail_price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'product': product,
+            'quantity': quantity,
+        })
+
+    
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- cart/views.py
+++ cart/views.py
@@ -11,7 +11,7 @@ def view_cart(request):
     return render(request, 'cart/cart.html')
 
 def add_to_cart(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    """ Add a quantity of the specified product to the shopping cart """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -23,5 +23,5 @@ def add_to_cart(request, item_id):
         cart[item_id] = quantity
 
     request.session['cart'] = cart
-    print(request.session['cart'])
+    
     return redirect(redirect_url)
