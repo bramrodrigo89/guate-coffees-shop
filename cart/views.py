09081f2f82f09de45cc7e06da208542143567659
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-08-15 18:07:03 +0000

    corrected cart table css and added update links functionality to update items in cart view

--- cart/views.py
+++ cart/views.py
@@ -4,7 +4,6 @@ from django.contrib import messages
 from products.models import Product
 
 
-# Create your views here.
 def view_cart(request):
     """ A view that renders all cart items """
 
@@ -12,6 +11,7 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
+
     quantity = int(request.POST.get('quantity'))
     grind = request.POST.get('grind')
     redirect_url = request.POST.get('redirect_url')
@@ -28,7 +28,23 @@ def add_to_cart(request, item_id):
         else:
             cart[item_id] = {'product_by_grind': {grind: quantity}}
         
-        print(cart)
         request.session['cart'] = cart
     
     return redirect(redirect_url)
+
+
+def update_cart(request, item_id):
+    """ Update item quantities in cart and delete them if requested """
+
+    quantity = int(request.POST.get('quantity'))
+    grind = request.POST.get('grind')
+    cart = request.session.get('cart', {})
+
+    if quantity > 0:
+        cart[item_id]['product_by_grind'][grind] = quantity
+    else:
+        del cart[item_id]['product_by_grind'][grind]
+
+    request.session['cart'] = cart
+    
+    return redirect(reverse(view_cart))
