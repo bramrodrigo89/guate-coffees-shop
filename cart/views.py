a18dee630409b08967f55a4b2fcfbdcddaa488da
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-08-18 02:07:20 +0000

    added messages to toasts when items are added, updated or deleted from cart

--- cart/views.py
+++ cart/views.py
@@ -12,6 +12,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     grind = request.POST.get('grind')
     redirect_url = request.POST.get('redirect_url')
@@ -23,10 +24,13 @@ def add_to_cart(request, item_id):
         if item_id in list(cart.keys()):
             if grind in cart[item_id]['product_by_grind'].keys():
                 cart[item_id]['product_by_grind'][grind] += quantity
+                messages.success(request, f'{product.name} ({grind.capitalize()} grind) added to your cart!')
             else:
                 cart[item_id]['product_by_grind'][grind] = quantity
+                messages.success(request, f'{product.name} ({grind.capitalize()} grind) added to your cart!')
         else:
             cart[item_id] = {'product_by_grind': {grind: quantity}}
+            messages.success(request, f'{product.name} added to your cart!')
         
         request.session['cart'] = cart
     
@@ -42,8 +46,10 @@ def update_cart(request, item_id):
 
     if quantity > 0:
         cart[item_id]['product_by_grind'][grind] = quantity
+        messages.success(request, 'Product updated in your cart!')
     else:
         del cart[item_id]['product_by_grind'][grind]
+        messages.success(request, 'Product deleted from your cart!')
         if not cart[item_id]['product_by_grind']:
             cart.pop(item_id)
 
@@ -60,7 +66,7 @@ def remove_from_cart(request, item_id):
         cart = request.session.get('cart', {})
 
         del cart[item_id]['product_by_grind'][grind]
-        print('maybe here?')
+        messages.success(request, 'Product deleted from your cart!')
         if not cart[item_id]['product_by_grind']:
             cart.pop(item_id)
 
