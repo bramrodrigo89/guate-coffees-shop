ac29cb64f9986cec6a2f63625315aabf1a515784
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-08-19 02:05:38 +0000

    added forms.py for order form and signals.py for updating order information such as order inline items

--- checkout/apps.py
+++ checkout/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class CheckoutConfig(AppConfig):
     name = 'checkout'
+
+    def ready(self):
+        """
+        Override ready method to import signals module: every time a line item is saved/deleted, order toal will be updated
+        """
+        import checkout.signals
