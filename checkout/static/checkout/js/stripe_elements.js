147b1262f62d9422dcedd64cdaf8dccab019e956
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-08-25 22:34:48 +0000

    added basic stripe functionality to checkout form and corrected css

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,8 +1,8 @@
 // STRIPE - Accept one-time payments
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
-var stripe = Stripe(stripe_public_key);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
     base: {
@@ -27,8 +27,8 @@ card.on("change", function (event) {
     var errorDiv = document.getElementById('card-errors');
     if (event.error) {
         var html = `
-            <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
+            <span role="alert">
+                <i class="material-icons left">error</i>
             </span>
             <span>${event.error.message}</span>
         `;
@@ -36,4 +36,40 @@ card.on("change", function (event) {
     } else {
         errorDiv.textContent = '';
     }
+});
+
+// Stripe Javascript to confirm credit card payment on form submit
+
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    // First disable card and submit button to avoid multiple form submits
+    card.update({ 'disabled': true });
+    $('#checkout-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function (result) {
+        if (result.error) {
+            // Show error to your customer (e.g., insufficient funds)
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span role="alert">
+                    <i class="material-icons left">error</i>
+                </span>
+                <span>${result.error.message}</span>
+            `;
+            $(errorDiv).html(html);
+            // Enable form submit button for customer to correct mistakes
+            card.update({ 'disabled': false });
+            $('#submit-button').attr('disabled', false);
+        } else {
+            // The payment has been processed!
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,45 @@
 from django.shortcuts import render, reverse, redirect
 from django.contrib import messages
-from .forms import OrderForm
+from django.conf import settings
 
+from .forms import OrderForm
+from cart.contexts import cart_items
+
+import stripe
 
 def checkout(request):
+    """
+    Create Payment Intent session at the beginning and rendering the checkout template with information needed for stripe payments
+    """
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    if not stripe_public_key:
+        messages.warning(request, 'Public Key is missing in environment! Please add Stripe Public Key')
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, 'There is nothing in your cart at this time')
         return redirect(reverse('products'))
     
+    current_cart = cart_items(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total*100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount = stripe_total,
+        currency = settings.STRIPE_CURRENCY
+    )
+
     order_form = OrderForm()
+
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51H3CrbDwaOGgZ3driENulN22GJQvWdg57GxbiLobaAee83s0SOGUFQQA4u0mbDwoRfotwe9w0NwlDim9bFU1q1cK00yNj7GFSv',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
