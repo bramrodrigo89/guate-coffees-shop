d51f7c14f18df0a933b51ee699b71f341ceb67c2
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-09-01 19:15:45 +0000

    changes on profile.html and corected errors in urls.py and views.py

--- profiles/views.py
+++ profiles/views.py
@@ -8,13 +8,13 @@ from checkout.models import Order
 @login_required
 def profile(request):
     """ Display the user's profile. """
-    profile = get_object_or_404(UserProfile, user=request.user)
-    orders = profile.orders.all()
+    # profile = get_object_or_404(UserProfile, user=request.user)
+    # orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
-        'orders': orders,
+        # 'profile': profile,
+        # 'orders': orders,
         'on_profile_page': True,
     }
 
