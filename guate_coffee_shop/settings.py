90a56027e0c7df0150ca61db141aaeb78a99e12b
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-09-02 22:22:35 +0000

    added heroku to allowed hosts in settings

--- guate_coffee_shop/settings.py
+++ guate_coffee_shop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['guatemalan-coffees-shop.herokuapp.com', 'localhost']
 
 # Application definition
 
