65d8d40d3ba45c3173d27aa68b0ba006f3e19e95
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-09-15 20:01:37 +0000

    added functionality to save user information when order is created using the webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 from .models import Order, OrderLineItem
 from products.models import Product
-# from profiles.models import UserProfile
+from profiles.models import UserInfo
 
 import json
 import time
@@ -42,19 +42,19 @@ class StripeWH_Handler:
                 shipping_details.address[field] = None
 
         # Update profile information if save_info was checked
-        # profile = None
+        user_info = None
         username = intent.metadata.username
-        # if username != 'AnonymousUser':
-        #     profile = UserProfile.objects.get(user__username=username)
-        #     if save_info:
-        #         profile.default_phone_number = shipping_details.phone
-        #         profile.default_country = shipping_details.address.country
-        #         profile.default_postcode = shipping_details.address.postal_code
-        #         profile.default_town_or_city = shipping_details.address.city
-        #         profile.default_street_address1 = shipping_details.address.line1
-        #         profile.default_street_address2 = shipping_details.address.line2
-        #         profile.default_county = shipping_details.address.state
-        #         profile.save()
+        if username != 'AnonymousUser':
+            user_info = UserInfo.objects.get(user__username=username)
+            if save_info:
+                user_info.default_phone_number = shipping_details.phone
+                user_info.default_postcode = shipping_details.address.postal_code
+                user_info.default_town_or_city = shipping_details.address.city
+                user_info.default_street_address_1 = shipping_details.address.line1
+                user_info.default_street_address_2 = shipping_details.address.line2
+                user_info.default_state = shipping_details.address.state
+                user_info.default_country = shipping_details.address.country
+                user_info.save()
 
         order_exists = False
         attempt = 1
@@ -88,7 +88,7 @@ class StripeWH_Handler:
                 order = Order.objects.create(
                     first_name=shipping_details.name.split(", ")[1],
                     last_name=shipping_details.name.split(", ")[0],
-                    username=username,
+                    username=user_info,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
