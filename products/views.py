57e7eb6a83bde1244292825d744793e23ed51e69
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-09-22 17:44:50 +0000

    added edit and delete views, modified widget html, added links in products.html to edit or delete

--- products/views.py
+++ products/views.py
@@ -130,3 +130,34 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        product_form = ProductForm(request.POST, request.FILES, instance=product)
+        if product_form.is_valid():
+            product_form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        product_form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': product_form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted from store!')
+    return redirect(reverse('products'))
