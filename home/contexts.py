d171bf784729e386a2aaa95820ec33fa1abce98c
Rodrigo Alvarez Monterroso <bramrodrigo89@gmail.com>
2020-10-14 04:54:18 +0000

    corrected last python format issue caused by docstring added

--- home/contexts.py
+++ home/contexts.py
@@ -5,8 +5,9 @@ from profiles.models import UserInfo
 
 def customer_inquiry_form(request):
     """
+
     This view is available in all apps via the context processors in order
-    to send inquiry forms from any template throughout the applicaiton
+    to send inquiry forms from any template throughout the application.
     """
     inquiry = CustomerInquiry()
     inquiry_form = CustomerInquiryForm()
